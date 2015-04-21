6cd4be84acd100f4de383fa5674a3899cc4e7cb3
emoen <endremoen@hotmail.com>
2015-04-21 08:21:24 +0200

    added controller and config to return json from http request

---
+++ src/main/java/no/nmdc/api/controller/MetadataController.java
@@ -0,0 +1,22 @@
+package no.nmdc.api.controller;
+
+import no.nmdc.api.MetadataApiImpl;
+import no.nmdc.solr.pojo.Facets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MetadataController {
+    
+    @Autowired
+    private MetadataApiImpl impl;
+    
+    @RequestMapping("/getFacets")
+    public @ResponseBody Facets getFacets() throws Exception {
+        System.out.println("getFacets");
+        return impl.getFacets(); 
+    }
+}
--- src/main/java/no/nmdc/web/config/WebMvcConfig.java
+++ src/main/java/no/nmdc/web/config/WebMvcConfig.java
@@ -3,16 +3,31 @@ package no.nmdc.web.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@ComponentScan(basePackages = {"no"})
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false).
+                favorParameter(true).
+                ignoreAcceptHeader(true).
+                parameterName("format").
+                useJaf(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML).
+                mediaType("json", MediaType.APPLICATION_JSON);
+    }
 
     /**
      * Override the defualt converters as we cannot control them and adds json
@@ -23,6 +38,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(getMappingJacksonHttpMessageConverter());
+                
     }
     
     /**
