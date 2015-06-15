17ae84efe865238a5414e0993c876aca054efc4d
emoen <endremoen@hotmail.com>
2015-06-15 14:31:06 +0200

    added search on toDate only

--- src/main/java/no/nmdc/solr/request/DateHelper.java
+++ src/main/java/no/nmdc/solr/request/DateHelper.java
@@ -7,21 +7,37 @@ import java.util.TimeZone;
 
 public class DateHelper {
     
+    private final String FIRST_RECORD = "1500-01-02T20:00:00Z";
+    
     public String createSolrDateQuerySyntax(String fromDate, String toDate) throws ParseException {
-    
-        SimpleDateFormat dateFormatUTC =    new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        dateFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
-    
-        Date start = dateFormatUTC.parse(fromDate);
-        
-        String range = "";
-        if ( toDate == null || toDate.equals("")  ) {
-            range = "[" + dateFormatUTC.format(start) + " *]";    
-        } else { 
-            Date end = dateFormatUTC.parse(toDate);
-            range = "[" + dateFormatUTC.format(start) + " TO " + dateFormatUTC.format(end) + "]";
+
+        if ( fromDate == null ) {
+            fromDate = "";
+        }
+        if ( toDate == null ) {
+            toDate = "";
         }
         
+        SimpleDateFormat dateFormatUTC = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        dateFormatUTC.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        Date start = null;
+        if ( !fromDate.equals("") ) {
+            start = dateFormatUTC.parse(fromDate);
+        }
+        Date end = null;
+        if ( !toDate.equals("") ) {
+            end = dateFormatUTC.parse(toDate);
+        }
+        
+        String range = "";
+        if ( toDate.equals("") && !fromDate.equals("") ) {
+            range = "[" + dateFormatUTC.format(start) + " *]";    
+        } else if ( fromDate.equals("") && !toDate.equals("") ) {
+            range = "[" + FIRST_RECORD + " TO " + dateFormatUTC.format(end) + "]";
+        } else if ( !fromDate.equals("") && !toDate.equals("") ){ 
+            range = "[" + dateFormatUTC.format(start) + " TO " + dateFormatUTC.format(end) + "]";
+        }
         return range;
     }
 }
--- src/test/java/no/nmdc/solr/request/DateHelperTest.java
+++ src/test/java/no/nmdc/solr/request/DateHelperTest.java
@@ -8,16 +8,18 @@ public class DateHelperTest {
     public void createSolrDateQuerySyntaxTest() throws Exception {
         DateHelper d = new DateHelper();
         String syntax = d.createSolrDateQuerySyntax("1901-01-02T20:00:00Z", "2013-01-02T20:00:00Z");
-        
         System.out.println(syntax);
         
         String syntax2 = d.createSolrDateQuerySyntax("1901-01-02T20:00:00Z", "");
-        
         System.out.println( syntax2 );
         
-        String syntax3 = d.createSolrDateQuerySyntax("1901-01-02T20:00:00Z", null);
-        
+        String syntax3 = d.createSolrDateQuerySyntax("1901-01-02T20:00:00Z", "");
         System.out.println( syntax3 );
+
+        String syntax4 = d.createSolrDateQuerySyntax("", "1936-01-02T20:00:00Z");
+        System.out.println( syntax4 );
         
+        String syntax5 = d.createSolrDateQuerySyntax(null, "1936-01-02T20:00:00Z");
+        System.out.println( syntax5 );
     }
 }
